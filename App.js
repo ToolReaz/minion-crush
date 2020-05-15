ea2619d585295d6629199f8518ca0baca13b744d
ToolReaz <toolreaz@outlook.fr>
2020-05-15 12:19:18 +0200

    Init

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text>Stanté</Text>
+        <Text>Faim</Text>
+        <Text>Humeur</Text>
+      </View>
+      <Image source={require("./assets/robot.png")} />
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity>
+        <Text>Actions</Text>
+      </TouchableOpacity>
+      <View>
+        <TextInput />
+        <TouchableOpacity>
+          <Text>></Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -12,8 +39,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
